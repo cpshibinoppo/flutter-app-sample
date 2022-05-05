a6eeb977dd00f86867963647f6576693c76d4302
cpshibinoppo <cpshibinoppo@gmail.com>
2022-05-05 18:04:03 +0530

    start

--- basics_dart/bin/basics_dart.dart
+++ basics_dart/bin/basics_dart.dart
@@ -7,9 +7,14 @@ void main() {
   print("Enter a two number");
   var input1 = stdin.readLineSync();
   var input2 = stdin.readLineSync();
+  var input3 = stdin.readLineSync();
 
   var num1 = int.parse(input1!);
   var num2 = int.parse(input2!);
+  var num3 = int.parse(input3!);
+  print("num1 $num1");
+  print("num2 $num2");
+  print("num3 $num3");
 
-  print('sum = ${num1 + num2}');
+  print('sum = ${num1 + num2 + num3}');
 }
