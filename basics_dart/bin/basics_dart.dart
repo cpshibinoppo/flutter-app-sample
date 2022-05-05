cd8b18c2a5d05b0fb5d8f99dff5932819cbe8e7f
cpshibinoppo <cpshibinoppo@gmail.com>
2022-05-05 17:49:20 +0530

    app start

---
+++ basics_dart/bin/basics_dart.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+// print('enter your name:');
+//  var a = stdin.readLineSync();
+//  print('Hello $a');
+  print("Enter a two number");
+  var input1 = stdin.readLineSync();
+  var input2 = stdin.readLineSync();
+
+  var num1 = int.parse(input1!);
+  var num2 = int.parse(input2!);
+
+  print('sum = ${num1 + num2}');
+}
